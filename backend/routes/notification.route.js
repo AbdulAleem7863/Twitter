ec081c545869632606b385745dd17008771f16b7
Abdul Aleem <abdulaleemnaqshbandi1234@gmail.com>
2024-08-28 18:52:31 +0500

    notification work completed

---
+++ backend/routes/notification.route.js
@@ -0,0 +1,12 @@
+import express from "express"
+import { protectRoute } from "../middleware/protectRoute.js"
+import { getNotification, deleteNotification } from '../controllers/notification.controller.js';
+
+
+const router = express.Router()
+
+router.get("/", protectRoute, getNotification)
+router.delete("/", protectRoute, deleteNotification)
+
+
+export default router
