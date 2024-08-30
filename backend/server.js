a0024aeb00336b69095d12534acdf95cf3fad181
Abdul Aleem <abdulaleemnaqshbandi1234@gmail.com>
2024-08-30 20:46:06 +0500

    some changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
+import cors from "cors"
 import cookieParser from 'cookie-parser'
 import { v2 as cloudinary } from "cloudinary"
 
@@ -21,10 +22,12 @@ cloudinary.config({
 
 const app = express()
 const port = process.env.PORT || 5000
-
 const __dirname = path.resolve()
 
+
+
 app.use(express.json({ limit: "5mb" }))
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
