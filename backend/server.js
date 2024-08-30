88dd216e427c6ae4c3767b9679427263f2b3a312
Abdul Aleem <abdulaleemnaqshbandi1234@gmail.com>
2024-08-30 20:16:03 +0500

    "Updated backend and frontend code: added path module, configured Cloudinary, updated server.js, modified package.json and package-lock.json files"

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import cookieParser from 'cookie-parser'
-import {v2 as cloudinary} from "cloudinary"
+import { v2 as cloudinary } from "cloudinary"
 
 
 import authRoutes from './routes/auth.route.js'
@@ -13,15 +14,17 @@ import connectMongoDB from './db/connectMongoDB.js'
 dotenv.config()
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(express.json({limit:"5mb"}))
+const __dirname = path.resolve()
+
+app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
@@ -29,6 +32,14 @@ app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
 app.use('/api/notifications', notificationRoutes)
+
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+
+}
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
